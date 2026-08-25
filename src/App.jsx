428d9ce5f91fbef780b226ef32dcc9c9e7dc4526
Dibeyendu Maity <dibeyendumaity@gmail.com>
2026-08-25 22:25:02 +0530

    Initial Navbar structure added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+
+            <main className="page-content">
+                <h1>Smart Parking</h1>
+                <p>Find your perfect parking spot.</p>
+            </main>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import { MapPin, Search, Heart, Moon } from "lucide-react";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+
+                {/* Logo */}
+                <Link to="/" className="logo">
+                    <MapPin size={24} />
+
+                    <span>
+                        Park<span>Ease</span>
+                    </span>
+                </Link>
+
+                {/* Navigation Links */}
+                <div className="nav-links">
+
+                    <Link to="/">
+                        Home
+                    </Link>
+
+                    <Link to="/search">
+                        <Search size={17} />
+                        <span>Find Parking</span>
+                    </Link>
+
+                    <Link to="/about">
+                        How It Works
+                    </Link>
+
+                    <Link to="/favorites">
+                        <Heart size={17} />
+                        <span>Favorites</span>
+                    </Link>
+
+                </div>
+
+                {/* Right Side */}
+                <div className="nav-actions">
+
+                    {/* Theme Button */}
+                    <button className="theme-btn">
+                        <Moon size={19} />
+                    </button>
+
+                    <Link to="/login" className="login-btn">
+                        Login
+                    </Link>
+
+                    <Link to="/register" className="register-btn">
+                        Get Started
+                    </Link>
+
+                </div>
+
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
