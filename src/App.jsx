ecac70e8a754e9927deacaf46bedb38916ef76b1
Dibeyendu Maity <dibeyendumaity@gmail.com>
2026-09-15 21:04:12 +0530

    Favoutites css and jsx file added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import MyBookings from "./pages/MyBookings";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import About from "./pages/About";
+import Favorites from "./pages/Favorites";
 
 function App() {
     return (
@@ -22,6 +23,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/favorites" element={<Favorites />} />
             </Routes>
         </>
     );
